55b4b435e99eaf6cf82ec3e44db58fb43e188c61
jyoti13698 <jkamble426@gmail.com>
2026-07-30 16:47:44 +0530

    Initialized sass for css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Dataset Explorer</h1>
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Dataset Explorer
+      </h1>
     </div>
   );
 }
