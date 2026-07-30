e9b82624c9764662b4b9aad10b215875bf8df4b4
jyoti13698 <jkamble426@gmail.com>
2026-07-30 17:04:57 +0530

    basic frontend folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-100">
-      <h1 className="text-5xl font-bold text-blue-600">
-        Dataset Explorer
-      </h1>
+    <div>
+      <h1>Dataset Explorer</h1>
     </div>
   );
 }
